8f967c1cdcec27e887ba9f1feefc63cb7b24f95a
Christian Blom <christian.blom@chasacademy.se>
2018-10-18 14:44:02 +0200

    First commit, started initializing variables and adding functions. Start game button has basic functions, correct amount of lette boxes shown.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Globala variabler
-
 var wordList; // Lista med spelets alla ord
 var selectedWord; // Ett av orden valt av en slumpgenerator
 var letterBoxes; //Rutorna där bokstäverna ska stå
@@ -12,15 +11,59 @@ var startTime; // Mäter tiden
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
-function init() {} // End init
+
+function init() {
+
+    wordList = [
+        "chas",
+        "academy",
+        "html",
+        "javascript",
+        "bootstrap",
+        "foundation",
+        "boilerplate",
+        "angular",
+        "react",
+        "firefox"
+    ];
+
+    selectedWord = randomWord(wordList);
+
+    startGameBtn = document.querySelector("#startGameBtn");
+
+    startGameBtn.addEventListener("click", startGame);
+
+    letterBoxes = document.querySelectorAll("#letterBoxes li");
+
+    console.log(selectedWord);
+
+    // setLetterBoxes(selectedWord);
+
+} // End init
 
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+function startGame() {
+    init();
+    setLetterBoxes(selectedWord);
+}
 
 // Funktion som slumpar fram ett ord
+function randomWord(wordArray) {
+    return wordArray[Math.floor(Math.random() * 10)];
+}
  
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
+function setLetterBoxes(word) {
+    for (var i = 0; i < 11; i++) {
+        letterBoxes[i].style.display = "none";
+    }
+
+    for (var i = 0; i < word.length; i++) {
+        letterBoxes[i].style.display = "inline-flex";
+    }
+}
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 
