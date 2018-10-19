1c79d15a7f88574e81518883065089139c74da3f
Christian Blom <christian.blom@chasacademy.se>
2018-10-19 18:45:56 +0200

    Letter buttons now add correct letters and get disabled when pressed. Start button resets letter buttons and boxes.

--- js/script.js
+++ js/script.js
@@ -33,19 +33,60 @@ function init() {
 
     startGameBtn.addEventListener("click", startGame);
 
-    letterBoxes = document.querySelectorAll("#letterBoxes li");
+    letterBoxes = document.querySelectorAll("#letterBoxes li input");
 
     letterButtons = document.querySelectorAll("#letterButtons button");
 
-    for (i = 0; i < letterButtons.length; i++) {
-        selectedLetter = letterButtons[i].value;
-        letterButtons[i].addEventListener("click", function(){writeLetterBox((selectedLetter))} );
-        console.log(selectedLetter);
+    for (var i = 0; i < letterButtons.length; i++) {
+        letterButtons[i].addEventListener("click", function (e) {
+            writeLetterBox(e);
+        })
     }
 
-    console.log(selectedWord);
+    // hangmanImg = [
+    //     h0.png,
+    //     h1.png,
+    //     h2.png,
+    //     h3.png,
+    //     h4.png,
+    //     h5.png,
+    //     h6.png
+    // ];
 
-    console.log(letterBoxes[2].firstElementChild.value);
+    // letterButtons.addEventListener("click", function(){writeLetterBox(letterButtons.value)})
+
+    // letterButtons[0].addEventListener("click", function(){writeLetterBox("A")} );
+    // letterButtons[1].addEventListener("click", function(){writeLetterBox("B")} );
+    // letterButtons[2].addEventListener("click", function(){writeLetterBox("C")} );
+    // letterButtons[3].addEventListener("click", function(){writeLetterBox("D")} );
+    // letterButtons[4].addEventListener("click", function(){writeLetterBox("E")} );
+    // letterButtons[5].addEventListener("click", function(){writeLetterBox("F")} );
+    // letterButtons[6].addEventListener("click", function(){writeLetterBox("G")} );
+    // letterButtons[7].addEventListener("click", function(){writeLetterBox("H")} );
+    // letterButtons[8].addEventListener("click", function(){writeLetterBox("I")} );
+    // letterButtons[9].addEventListener("click", function(){writeLetterBox("J")} );
+    // letterButtons[10].addEventListener("click", function(){writeLetterBox("K")} );
+    // letterButtons[11].addEventListener("click", function(){writeLetterBox("L")} );
+    // letterButtons[12].addEventListener("click", function(){writeLetterBox("M")} );
+    // letterButtons[13].addEventListener("click", function(){writeLetterBox("N")} );
+    // letterButtons[14].addEventListener("click", function(){writeLetterBox("O")} );
+    // letterButtons[15].addEventListener("click", function(){writeLetterBox("P")} );
+    // letterButtons[16].addEventListener("click", function(){writeLetterBox("Q")} );
+    // letterButtons[17].addEventListener("click", function(){writeLetterBox("R")} );
+    // letterButtons[18].addEventListener("click", function(){writeLetterBox("S")} );
+    // letterButtons[19].addEventListener("click", function(){writeLetterBox("T")} );
+    // letterButtons[20].addEventListener("click", function(){writeLetterBox("U")} );
+    // letterButtons[21].addEventListener("click", function(){writeLetterBox("V")} );
+    // letterButtons[22].addEventListener("click", function(){writeLetterBox("W")} );
+    // letterButtons[23].addEventListener("click", function(){writeLetterBox("X")} );
+    // letterButtons[24].addEventListener("click", function(){writeLetterBox("Y")} );
+    // letterButtons[25].addEventListener("click", function(){writeLetterBox("Z")} );
+    // letterButtons[26].addEventListener("click", function(){writeLetterBox("Å")} );
+    // letterButtons[27].addEventListener("click", function(){writeLetterBox("Ä")} );
+    // letterButtons[28].addEventListener("click", function(){writeLetterBox("Ö")} );
+
+
+    console.log(selectedWord);
 
 } // End init
 
@@ -55,6 +96,7 @@ window.onload = init; // Se till att init aktiveras då sidan är inladdad
 function startGame() {
     init();
     setLetterBoxes(selectedWord);
+    reactivateButtons();
 }
 
 // Funktion som slumpar fram ett ord
@@ -70,14 +112,36 @@ function setLetterBoxes(word) {
 
     for (var i = 0; i < word.length; i++) {
         letterBoxes[i].style.display = "inline-flex";
+        letterBoxes[i].value = " ";
     }
 }
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 function writeLetterBox(selectedLetter) {
-    console.log(selectedLetter);
+
+    var foundLetter = false;
+
+    for (var i = 0; i < selectedWord.length; i++) {
+        if (selectedWord.charAt(i) == selectedLetter.target.value) {
+            letterBoxes[i].value = selectedLetter.target.value;
+            foundLetter = true;
+        } 
+    }
+
+    if (foundLetter === false) {
+
+    }
+
+    selectedLetter.target.disabled = true;
+    
+    console.log(selectedLetter.target.value);
 }
 
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
 
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+function reactivateButtons() {
+    for (var i = 0; i < letterButtons.length; i++) {
+        letterButtons[i].disabled = false;
+    }
+}
